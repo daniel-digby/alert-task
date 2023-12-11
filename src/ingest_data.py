85ef81856d98c64136933c1765065bdfcd96433e
Daniel Digby <daniel.a.digby@gmail.com>
2023-12-11 21:48:46 +0000

    feat: implement ingest_data

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -2,4 +2,7 @@ import sqlalchemy as sa
 
 
 def ingest_data(conn: sa.Connection, timestamp: str, event_type: str):
-    ...
+    conn.execute(
+        sa.text("INSERT INTO events (time, type) VALUES (:timestamp, :event_type)"),
+        {"timestamp": timestamp, "event_type": event_type},
+    )
