d4cd8b5492af35e627409a2a60c7137855c2b45a
Daniel Digby <daniel.a.digby@gmail.com>
2023-12-11 21:59:37 +0000

    feat: add timestamp validation to ingest_data

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,7 +1,18 @@
+from datetime import datetime
 import sqlalchemy as sa
 
 
-def ingest_data(conn: sa.Connection, timestamp: str, event_type: str):
+def validate_timestamp(timestamp: str):
+    try:
+        datetime.fromisoformat(timestamp)
+    except ValueError as e:
+        raise ValueError(
+            "Invalid timestamp format. Must be an isoformat string."
+        ) from e
+
+
+def ingest_data(conn: sa.Connection, timestamp: str, event_type: str) -> None:
+    validate_timestamp(timestamp)
     conn.execute(
         sa.text("INSERT INTO events (time, type) VALUES (:timestamp, :event_type)"),
         {"timestamp": timestamp, "event_type": event_type},
