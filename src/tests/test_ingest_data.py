dce4d8a5c9f63b13932b2731e148e52d0bc124d4
Daniel Digby <daniel.a.digby@gmail.com>
2023-12-11 22:39:05 +0000

    tests: adjust tests to account for bulk events

--- src/tests/test_ingest_data.py
+++ src/tests/test_ingest_data.py
@@ -7,15 +7,6 @@ from src.ingest_data import ingest_data
 
 class TestIngestData(BaseTestCase):
     def test_valid_input(self):
-        timestamp = "2023-08-10T18:30:30"
-        event_type = "pedestrian"
-
-        ingest_data(self.conn, timestamp, event_type)
-
-        result = self.conn.execute(sa.text("SELECT * FROM events")).fetchall()
-        self.assertEqual(len(result), 1)
-
-    def test_multiple_valid_inputs(self):
         events = [
             ("2023-08-10T18:30:30", "pedestrian"),
             ("2023-08-10T18:31:00", "pedestrian"),
@@ -28,26 +19,30 @@ class TestIngestData(BaseTestCase):
             ("2023-08-10T18:37:30", "pedestrian"),
         ]
 
-        for timestamp, event_type in events:
-            ingest_data(self.conn, timestamp, event_type)
+        ingest_data(self.conn, events)
 
         result = self.conn.execute(sa.text("SELECT * FROM events")).fetchall()
         self.assertEqual(len(result), len(events))
 
     def test_invalid_timestamp(self):
-        timestamp = "Invalid Timestamp"
-        event_type = "pedestrian"
-
-        with self.assertRaises(ValueError) as error:
-            ingest_data(self.conn, timestamp, event_type)
-
-        self.assertIn("timestamp", str(error.exception))
+        events = [
+            ("Invalid Timestamp", "pedestrian"),
+        ]
+        invalid_events = ingest_data(self.conn, events)
+        self.assertTrue(invalid_events[0]["timestamp"] == "Invalid Timestamp")
 
     def test_invalid_event_type(self):
-        timestamp = "2023-08-10T18:30:30"
-        event_type = "Invalid Event Type"
+        events = [
+            ("2023-08-10T18:30:30", "Invalid Event Type"),
+        ]
+        invalid_events = ingest_data(self.conn, events)
+        self.assertTrue(invalid_events[0]["event_type"] == "Invalid Event Type")
 
-        with self.assertRaises(ValueError) as error:
-            ingest_data(self.conn, timestamp, event_type)
-
-        self.assertIn("event type", str(error.exception))
+    def test_multiple_invalid_events(self):
+        events = [
+            ("Invalid Timestamp", "pedestrian"),
+            ("2023-08-10T18:30:30", "Invalid Event Type"),
+        ]
+        invalid_events = ingest_data(self.conn, events)
+        self.assertTrue(invalid_events[0]["timestamp"] == "Invalid Timestamp")
+        self.assertTrue(invalid_events[1]["event_type"] == "Invalid Event Type")
