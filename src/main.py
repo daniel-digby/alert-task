50406cc2fbbddd06016df8a2e226cc180c7f1585
Daniel Digby <daniel.a.digby@gmail.com>
2023-12-11 21:16:29 +0000

    refactor: extract db module

--- src/main.py
+++ src/main.py
@@ -1,27 +1,5 @@
-import time
-
+from db import database_connection
 import sqlalchemy as sa
-
-
-def database_connection() -> sa.Connection:
-    engine = sa.create_engine("postgresql://postgres:postgres@postgres:5432/postgres")
-
-    for attempt in range(5):
-        try:
-            conn = engine.connect()
-        except sa.exc.OperationalError as e:
-            if attempt == 4:
-                raise e
-            time.sleep(1)
-
-    conn.execute(
-        sa.text(
-            "CREATE TABLE IF NOT EXISTS events "
-            "(id SERIAL PRIMARY KEY, time TIMESTAMP WITH TIME ZONE, type VARCHAR)"
-        )
-    )
-
-    return conn
 
 
 def ingest_data(conn: sa.Connection, timestamp: str, event_type: str):
